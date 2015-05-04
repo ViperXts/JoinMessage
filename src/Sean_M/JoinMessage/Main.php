59140a2a191da1005640d68cf6e1378255b54e88
seanmcpe <seanmcpex@gmail.com>
2015-05-04 15:46:28 -0400

    Update Main.php

--- src/Sean_M/JoinMessage/Main.php
+++ src/Sean_M/JoinMessage/Main.php
@@ -1,9 +1,9 @@
 <?php
 namespace Sean_M\JoinMessage;
- 
+
+use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
       
 class Main extends PluginBase implements Listener{
@@ -13,8 +13,8 @@ class Main extends PluginBase implements Listener{
      $this->getLogger()->info("JoinMessage enabled!");
      }
 
-     public function onPlayerJoin(PlayerJoinEvent $event){
-     $p = $event->getPlayer();
-     $p->sendMessage("Welcome to my server!");
-     }
+       public function onPlayerJoin(PlayerJoinEvent $event){
+       $p = $event->getPlayer();
+       $p->sendMessage("Welcome to my server!");
+       }
 }
